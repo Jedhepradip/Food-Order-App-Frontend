a51a8d81d2662ea361b0e5da3fe7cf876f5b8f3d
pradip jedhe <pradipjedhe69@gmail.com>
2024-12-15 15:55:08 +0530

    add the

--- src/Pages/NotFound.tsx
+++ src/Pages/NotFound.tsx
@@ -1,7 +1,7 @@
 // src/components/NotFound.jsx
 import React from "react";
 
-const NotFound:React.FC = () => {
+const NotFound: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-red-500">404</h1>
