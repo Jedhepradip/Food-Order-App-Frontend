ce4088c9e3725dde0d93281ceda1a8a110a8d0e7
pradip jedhe <pradipjedhe69@gmail.com>
2025-01-07 20:54:49 +0530

    All Competed website

--- src/Pages/Filtercuisines.tsx
+++ src/Pages/Filtercuisines.tsx
@@ -8,6 +8,7 @@ import { SetFilterCuisines } from '../Redux/Features/CuisinesFilterSlice';
 const Filtercuisines: React.FC = () => {
     const [AllRestaurantData, setAllRestaurantData] = useState<RestaurantInterface[]>()
     const [allCuisines, setAllCuisines] = useState<string[]>([]);
+    const [Cuisines, Cuisinesdata] = useState<string[]>([]);
     const dispatch: AppDispatch = useDispatch()
     const RestureantdataAll = useSelector((state: RootState) => state.AllRestaurant.RestaurantAll)
 
@@ -42,8 +43,20 @@ const Filtercuisines: React.FC = () => {
         dispatch(SetFilterCuisines([{ Cuisines: "" }]));
     }
 
+    // console.log(allCuisines.filter());
+
+    useEffect(() => {
+        const data = allCuisines?.filter((item: string) => item.trim() !== '')
+        console.log(data);
+        Cuisinesdata(data)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    console.log("Cuisines ", Cuisines);
+
+
     return (
-        
+
         <div className="bg-black w-full text-white md:w-72 h-auto rounded-lg shadow-lg p-5 md:p-0 mt-6 md:mt-0">
             <div className="flex justify-between items-center mb-6 border-b border-gray-700 pb-2">
                 <h1 className="text-xl font-bold tracking-wide">Filter By Cuisines</h1>
@@ -52,7 +65,7 @@ const Filtercuisines: React.FC = () => {
                 </h2>
             </div>
             <div className="space-y-3">
-                {allCuisines.map((val, index) => (
+                {Cuisines.map((val, index) => (
                     <div
                         key={index}
                         className="flex items-center p-2 bg-gray-800 rounded-lg transition-all duration-150 hover:bg-gray-700 focus-within:bg-gray-700"
@@ -66,7 +79,7 @@ const Filtercuisines: React.FC = () => {
                         />
                         <label
                             htmlFor={`cuisine-${index}`}
-                            className="ml-3 text-sm font-medium capitalize cursor-pointer"
+                            className="ml-3 text-sm font-medium  capitalize cursor-pointer"
                         >
                             {val}
                         </label>
